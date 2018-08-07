6ff0da1b068c15eef743332b6b1a8e5c387d9dec
Jonathan Furr <Jonfurr@gmail.com>
2018-08-07 11:31:43 -0700

    initial commit

---
+++ utils/colors.js
@@ -0,0 +1,11 @@
+// utils/colors.js
+
+export const black = '#0c0c0c'
+export const gray = '#757575'
+export const white = '#f8f8f8'
+export const green = '#047200'
+export const red = '#ca0e0c'
+export const darkGray = '#333'
+export const lightGray = '#ccc'
+export const blue = '#34c6da'
+export const orange = '#fbab5a'
